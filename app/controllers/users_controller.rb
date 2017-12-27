868c9048141c03cbbb6a444755c5849fc5e8dbff
enta0701 <endo.takuya.0701@gmail.com>
2017-12-27 10:06:09 +0900

    use includes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.includes(:profile)
   end
 
   # GET /users/1
